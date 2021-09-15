8df79ce986586cd1b20624dcb725ff9bc7697a6f
Dave Hoy <david.hoy@techelevator.com>
2021-09-15 12:13:59 -0400

    M1D3 Lecture final and exercises

---
+++ module-1/03_Expressions/lecture-final/java/src/main/java/com/techelevator/QuizStuff.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+public class QuizStuff {
+    public static void main(String[] args) {
+        int x = (int)(7.0 / 4);
+        System.out.println(x);
+
+        double y = (5 / 2) * 4;
+        System.out.println(y);
+
+        // Write a constant variable definition for pi, and assign it a value of 3.14.
+        final float PI = 3.14F;
+    }
+}
