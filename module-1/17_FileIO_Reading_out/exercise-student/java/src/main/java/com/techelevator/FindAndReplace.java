537bc48a9b5b53553d6ed1a098485c8e8b7aed67
Tay Black <taylor.gr.black@gmail.com>
2021-10-05 17:30:04 -0400

    Submitted Week 4 Day 17 exercise

--- module-1/17_FileIO_Reading_out/exercise-student/java/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_FileIO_Reading_out/exercise-student/java/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,8 +1,81 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
     public static void main(String[] args) {
+
+        //ask for word to search for
+        //ask for word to replace search word
+        //ask for source file
+        //ask for destination file
+        //logic.do
+        try (Scanner userInput = new Scanner(System.in)) {
+            System.out.println("Please input word to search for");
+            String wordToSearch = userInput.nextLine();
+            if (wordToSearch.isEmpty() || wordToSearch == null) {
+                System.out.println("ERROR: Given input invalid. Do better.");
+                System.exit(0);
+            }
+            System.out.println("Please input word to replace");
+            String replaceWord = userInput.nextLine();
+            if (replaceWord.isEmpty() || replaceWord == null) {
+                System.out.println("ERROR: Given input invalid. Do better.");
+                System.exit(0);
+            }
+            System.out.println("Please input qualified folder path >>>>  ");
+            String path = userInput.nextLine();
+            File sourceFile = new File(path);
+            if (!sourceFile.exists()) {
+                System.out.println("Error: " + path + " no file found.");
+                System.exit(0);
+            } else if (!sourceFile.isFile()) {
+                System.out.println("You really need a file here.");
+                System.exit(0);
+            }
+            System.out.println("Please input a qualified destination folder path >>> ");
+            path = userInput.nextLine();
+            File destinationFile = new File(path);
+            if (!destinationFile.exists()) {
+                System.out.println("Error: " + path + " no file found.");
+                System.exit(0);
+            }
+            if (!destinationFile.getName().endsWith(".txt")) {
+                System.out.println("Destination file MUST be a text (.txt) file.");
+                System.exit(0);
+            }
+            //do logic:
+            try(Scanner readSourceFile = new Scanner(sourceFile.getAbsoluteFile());
+                PrintWriter printWriterDestination = new PrintWriter(destinationFile.getAbsoluteFile())){
+                //take the search word
+                //take the replace word
+                //swap them
+                while(readSourceFile.hasNextLine()){
+                    String line = readSourceFile.nextLine();
+                    printWriterDestination.println(line.replaceAll(wordToSearch,replaceWord));
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+
+
+        //boolean is invalid...maybe make an entire repeatable program?
+        //nullpointer exception
+        //file i/o exception
+        //invalid argument exception
+        //taking user input file
+        //writing to a new file
+        //if inputfile doesn't exist
+        //if destination file doesn't exist
+        //variables for both maybe
+        //feeling weird today
+
 
     }
 
--- module-1/17_FileIO_Reading_out/exercise-student/java/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_FileIO_Reading_out/exercise-student/java/src/main/java/com/techelevator/FizzWriter.java
@@ -1,8 +1,59 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FizzWriter {
 
 	public static void main(String[] args) {
+		//get file
+		//get destination file
+		//for loop while i <=300
+		//counter
+		//print to destination file
+		try(Scanner userInput = new Scanner(System.in)) {
+			System.out.println("Please input a qualified destination folder path >>> ");
+			String path = userInput.nextLine();
+			File destinationFile = new File(path);
+			if(path.isEmpty() || path == null){
+				System.out.printf("The file is empty. Please fix. ");
+				System.exit(0);
+			}
+			if (!destinationFile.getName().endsWith(".txt")) {
+				System.out.println("Destination file MUST be a text (.txt) file.");
+				System.exit(0);
+			}
+			try(PrintWriter printWriterDestination = new PrintWriter(destinationFile.getAbsoluteFile())){
+				for(int i = 1; i <= 300; i++){
+					if(i % 3 == 0 && i % 5 == 0){
+						printWriterDestination.println("FizzBuzz");
+					} else if( i % 5 == 0) {
+						printWriterDestination.println("Buzz");
+					} else if (i % 3 == 0){
+						printWriterDestination.println("Fizz");
+					} else {
+						printWriterDestination.println(i);
+					}
+				}
+
+			}catch(FileNotFoundException e){
+				System.out.println("File" + path + "not found");
+				System.exit(0);
+			}
+
+
+		}catch(Exception e){
+			System.out.println("Not allowed. ");
+			System.exit(0);
+
+		}
+
+
+
+
+
 
 	}
 
