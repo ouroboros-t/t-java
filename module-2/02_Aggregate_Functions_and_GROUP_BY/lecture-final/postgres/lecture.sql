506184d4a5ff93ee9856db33b494173c631c2598
Dave Hoy <david.hoy@techelevator.com>
2021-10-12 14:03:26 -0400

    M2D2 Lecture final

---
+++ module-2/02_Aggregate_Functions_and_GROUP_BY/lecture-final/postgres/lecture.sql
@@ -0,0 +1,78 @@
+-- ORDERING RESULTS
+
+-- Populations of all states from largest to smallest.
+
+
+-- States sorted alphabetically (A-Z) within their census region. The census regions are sorted in reverse alphabetical (Z-A) order.
+
+
+-- The biggest parks by area
+
+
+
+-- LIMITING RESULTS
+
+-- The 10 largest cities by populations
+
+
+-- The 20 oldest parks from oldest to youngest in years then alphabetically 
+
+
+
+
+-- CONCATENATING OUTPUTS
+
+-- All city names and their state abbreviation as a single column result set
+
+
+-- The all parks by name and date established as a single column result set
+
+
+
+-- AGGREGATE FUNCTIONS
+
+-- Average population across all the states. Note the use of alias, common with aggregated values.
+
+
+-- Total population in the West and South census regions
+
+
+-- The number of cities with populations greater than 1 million
+
+
+-- The number of state nicknames.
+
+
+-- The area of the smallest and largest parks.
+
+
+
+-- GROUP BY
+
+-- Count the number of cities in each state, ordered from most cities to least.
+
+
+-- Determine the average park area depending upon whether parks allow camping or not.
+
+
+-- Sum of the population of cities in each state ordered by state abbreviation.
+
+
+-- The smallest city population in each state ordered by city population.
+
+
+
+-- Miscelleneous
+
+-- While you can use LIMIT to limit the number of results returned by a query,
+-- it's recommended to use OFFSET you want to get "pages" of results.
+
+
+
+-- SUBQUERIES (optional)
+
+-- All of the cities in the Midwest census region
+
+
+-- All of the cities with their full state name
+
