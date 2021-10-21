301112952e76b2a3850b21f273e7ead78f7ed723
Tay Black <taylor.gr.black@gmail.com>
2021-10-21 15:18:35 -0400

    Submitted Module 2 Day 9 exercise

--- module-2/09_Integration_Testing/exercise-student/java/src/test/java/com/techelevator/projects/dao/JdbcTimesheetDaoTests.java
+++ module-2/09_Integration_Testing/exercise-student/java/src/test/java/com/techelevator/projects/dao/JdbcTimesheetDaoTests.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.Time;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -18,58 +19,122 @@ public class JdbcTimesheetDaoTests extends BaseDaoTests {
             LocalDate.parse("2021-01-01"), 0.25, true, "Timesheet 3");
     private static final Timesheet TIMESHEET_4 = new Timesheet(4L, 2L, 2L,
             LocalDate.parse("2021-02-01"), 2.0, false, "Timesheet 4");
-    
+
+    private Timesheet testTimesheet;
+
     private JdbcTimesheetDao sut;
 
 
     @Before
     public void setup() {
         sut = new JdbcTimesheetDao(dataSource);
+        testTimesheet = new Timesheet(0L,1L, 2L, LocalDate.parse("2021-10-21"), 3.5, true, "Test Timesheet");
     }
 
     @Test
     public void getTimesheet_returns_correct_timesheet_for_id() {
-        Assert.fail();
+        Timesheet timesheet = sut.getTimesheet(1L);
+        assertTimesheetsMatch(TIMESHEET_1, timesheet);
+
+        timesheet = sut.getTimesheet(3L);
+        assertTimesheetsMatch(TIMESHEET_3,timesheet);
     }
 
     @Test
     public void getTimesheet_returns_null_when_id_not_found() {
-        Assert.fail();
+        Timesheet timesheet = sut.getTimesheet(-9L);
+        Assert.assertNull(timesheet);
+
+        timesheet = sut.getTimesheet(8L);
+        Assert.assertNull(timesheet);
     }
 
     @Test
     public void getTimesheetsByEmployeeId_returns_list_of_all_timesheets_for_employee() {
-        Assert.fail();
+        List<Timesheet> timesheets = sut.getTimesheetsByEmployeeId(1L);
+        Assert.assertEquals(2,timesheets.size());
+
+        assertTimesheetsMatch(TIMESHEET_1, timesheets.get(0));
+        assertTimesheetsMatch(TIMESHEET_2, timesheets.get(1));
+
     }
 
     @Test
     public void getTimesheetsByProjectId_returns_list_of_all_timesheets_for_project() {
-        Assert.fail();
+        List<Timesheet> timesheets = sut.getTimesheetsByProjectId(1L);
+        Assert.assertEquals(3,timesheets.size());
+
+        assertTimesheetsMatch(TIMESHEET_1, timesheets.get(0));
+        assertTimesheetsMatch(TIMESHEET_2, timesheets.get(1));
+        assertTimesheetsMatch(TIMESHEET_3, timesheets.get(2));
+
+
     }
 
     @Test
     public void createTimesheet_returns_timesheet_with_id_and_expected_values() {
-        Assert.fail();
+        Timesheet createdTimesheet = sut.createTimesheet(testTimesheet);
+
+        long newId = createdTimesheet.getTimesheetId();
+        Assert.assertTrue(newId > 0);
+
+        testTimesheet.setTimesheetId(newId);
+       assertTimesheetsMatch(testTimesheet,createdTimesheet);
     }
 
     @Test
     public void created_timesheet_has_expected_values_when_retrieved() {
-        Assert.fail();
+        Timesheet createdTimesheet = sut.createTimesheet(testTimesheet);
+
+        long newId = createdTimesheet.getTimesheetId();
+        Timesheet retrieveTimesheet = sut.getTimesheet(newId);
+
+        testTimesheet.setTimesheetId(newId);
+        assertTimesheetsMatch(testTimesheet,retrieveTimesheet);
+
     }
 
     @Test
     public void updated_timesheet_has_expected_values_when_retrieved() {
-        Assert.fail();
+        Timesheet timesheetToUpdate = sut.getTimesheet(1L);
+        timesheetToUpdate.setEmployeeId(2L);
+        timesheetToUpdate.setProjectId(2L);
+        timesheetToUpdate.setDateWorked(LocalDate.parse("2021-10-21"));
+        timesheetToUpdate.setHoursWorked(3.7);
+        timesheetToUpdate.setBillable(false);
+        timesheetToUpdate.setDescription("This is a test.");
+
+        sut.updateTimesheet(timesheetToUpdate);
+
+        Timesheet retrieveTimesheet = sut.getTimesheet(1L);
+
+        assertTimesheetsMatch(timesheetToUpdate,retrieveTimesheet);
+
     }
 
     @Test
     public void deleted_timesheet_cant_be_retrieved() {
-        Assert.fail();
+        sut.deleteTimesheet(1L);
+
+        Timesheet retrievedTimesheet = sut.getTimesheet(1L);
+        Assert.assertNull(retrievedTimesheet);
     }
 
     @Test
     public void getBillableHours_returns_correct_total() {
-        Assert.fail();
+        double expected = 2.5;
+        double actual = sut.getBillableHours(1L,1L);
+        Assert.assertEquals(expected,actual,0);
+
+        expected = 0.25;
+        actual = sut.getBillableHours(2L,1L);
+        Assert.assertEquals(expected,actual,0);
+
+        expected = 0.0;
+        actual = sut.getBillableHours(2L, 2L);
+        Assert.assertEquals(expected,actual,0);
+
+
     }
 
     private void assertTimesheetsMatch(Timesheet expected, Timesheet actual) {
