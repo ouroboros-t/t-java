519feee53fe2ca8e46df4ce91cb7d80274a19e9e
Tay Black <taylor.gr.black@gmail.com>
2021-10-25 15:29:35 -0400

    Submitted M2D11 exercise

--- module-2/11_HTTP_Web_Services_GET/exercise-student/java/src/main/java/com/techelevator/auctions/services/AuctionService.java
+++ module-2/11_HTTP_Web_Services_GET/exercise-student/java/src/main/java/com/techelevator/auctions/services/AuctionService.java
@@ -1,5 +1,6 @@
 package com.techelevator.auctions.services;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import com.techelevator.auctions.model.Auction;
@@ -12,22 +13,30 @@ public class AuctionService {
 
     public Auction[] getAllAuctions() {
         // call api here
-        return null;
+        String url = API_BASE_URL;
+        Auction[] auctions = restTemplate.getForObject(url,Auction[].class);
+        return auctions;
     }
 
     public Auction getAuction(int id) {
         // call api here
-        return null;
+        String url = API_BASE_URL + id;
+        Auction auction = restTemplate.getForObject(url,Auction.class);
+        return auction;
     }
 
     public Auction[] getAuctionsMatchingTitle(String title) {
         // call api here
-        return null;
+        String url = API_BASE_URL +"?title_like=" + title;
+        Auction[] auctions = restTemplate.getForObject(url,Auction[].class);
+        return auctions;
     }
 
     public Auction[] getAuctionsAtOrBelowPrice(double price) {
         // call api here
-        return null;
+        String url = API_BASE_URL+ "?currentBid_lte=" + price;
+        Auction[] auctions = restTemplate.getForObject(url,Auction[].class);
+        return auctions;
     }
 
 }
