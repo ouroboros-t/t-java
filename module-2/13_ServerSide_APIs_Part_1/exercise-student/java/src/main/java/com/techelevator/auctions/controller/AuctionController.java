d30bdd0381208c58903bf54d89c1e33f0ad2c937
Tay Black <taylor.gr.black@gmail.com>
2021-10-27 18:44:27 -0400

    Submitted M2D13 exercise

--- module-2/13_ServerSide_APIs_Part_1/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_ServerSide_APIs_Part_1/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,7 +2,10 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/auctions")
@@ -13,5 +16,44 @@ public class AuctionController {
     public AuctionController() {
         this.dao = new MemoryAuctionDao();
     }
+
+
+    @GetMapping()
+    public List<Auction> listAuction(@RequestParam(required = false, defaultValue = "") String title_like,
+                                      @RequestParam(required = false, defaultValue = "0") double currentBid_lte){
+
+        if(!title_like.equals("") && currentBid_lte !=0){
+            return dao.searchByTitleAndPrice(title_like,currentBid_lte);
+        } else if(!title_like.equals("")){
+            return dao.searchByTitle(title_like);
+        } else if (currentBid_lte != 0) {
+            return dao.searchByPrice(currentBid_lte);
+        }
+        return dao.list();
+    }
+
+    @GetMapping("/{id}")
+    public Auction getAuction(@PathVariable int id){
+        return dao.get(id);
+    }
+
+    @PostMapping()
+    public Auction createAuction(@RequestBody Auction newAuction){
+        return dao.create(newAuction);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
