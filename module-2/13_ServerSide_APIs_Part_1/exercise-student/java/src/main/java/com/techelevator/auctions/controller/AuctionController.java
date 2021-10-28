410934841be98d003dd54c70753319020c2c09da
Dave Hoy <david.hoy@techelevator.com>
2021-10-28 13:46:08 -0400

    M2D14 Lecture final

--- module-2/13_ServerSide_APIs_Part_1/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_ServerSide_APIs_Part_1/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,6 +2,7 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,6 +13,14 @@ public class AuctionController {
 
     public AuctionController() {
         this.dao = new MemoryAuctionDao();
+    }
+
+    /**
+     * Return the Auction with the given ID
+     */
+    @RequestMapping(path = "/{auctionId}", method = RequestMethod.GET)
+    public Auction getById(@PathVariable int auctionId) {
+        return dao.get(auctionId);
     }
 
 }
