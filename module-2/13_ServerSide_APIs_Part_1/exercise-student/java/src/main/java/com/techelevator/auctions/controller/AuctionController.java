c801b718e5345782225ec6a682ad29ccd51313c4
Dave Hoy <david.hoy@techelevator.com>
2021-10-29 07:29:02 -0400

    M2D13 Reverted exercise-student changes (oops)

--- module-2/13_ServerSide_APIs_Part_1/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_ServerSide_APIs_Part_1/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,7 +2,6 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
-import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,14 +12,6 @@ public class AuctionController {
 
     public AuctionController() {
         this.dao = new MemoryAuctionDao();
-    }
-
-    /**
-     * Return the Auction with the given ID
-     */
-    @RequestMapping(path = "/{auctionId}", method = RequestMethod.GET)
-    public Auction getById(@PathVariable int auctionId) {
-        return dao.get(auctionId);
     }
 
 }
