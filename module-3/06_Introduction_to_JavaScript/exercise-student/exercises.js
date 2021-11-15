96f05befaefd67c9b012ee55edd88d9efb78e823
Tay Black <taylor.gr.black@gmail.com>
2021-11-15 16:04:56 -0500

    last two problems unfinished/incorrect

--- module-3/06_Introduction_to_JavaScript/exercise-student/exercises.js
+++ module-3/06_Introduction_to_JavaScript/exercise-student/exercises.js
@@ -1,74 +1,125 @@
 ﻿/*
 1. **sumDouble** Given two int values, return their sum. Unless the two values are the 
-    same, then return double their sum.
+	same, then return double their sum.
 
 		sumDouble(1, 2) → 3
 		sumDouble(3, 2) → 5
 		sumDouble(2, 2) → 8
-
-		function sumDouble(x, y) {
-			// do logic here
-			// return result;
-			return x + y;
-        }
 */
+function sumDouble(x, y) {
+	if (x === y) {
+		return 2 * (x + y);
+	} else {
+		// return result;
+		return x + y;
+	}
+}
 
 /*
-2. **hasTeen** We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
-    Given 3 int values, return true if 1 or more of them are teen.
+2. **hasTeen** We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+	Given 3 int values, return true if 1 or more of them are teen.
 
 		hasTeen(13, 20, 10) → true
 		hasTeen(20, 19, 10) → true
 		hasTeen(20, 10, 13) → true
 */
 
-/* 
-3. **lastDigit** Given two non-negative int values, return true if they have the same 
-    last digit, such as with 27 and 57.
+function hasTeen(x, y, z) {
+	if (x >= 13 && x >= 19 || y >= 13 && y >= 19 || z >= 13 && z >= 19) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/*
+3. **lastDigit** Given two non-negative int values, return true if they have the same
+	last digit, such as with 27 and 57.
 
 		lastDigit(7, 17) → true
 		lastDigit(6, 17) → false
 		lastDigit(3, 113) → true
 */
 
+function lastDigit(x, y) {
+	if (x % 10 === y % 10) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
 /*
-4. **seeColor** Given a string, if the string begins with "red" or "blue" return that color 
-    string, otherwise return the empty string.
+4. **seeColor** Given a string, if the string begins with "red" or "blue" return that color
+	string, otherwise return the empty string.
 
 		seeColor("redxx") → "red"
 		seeColor("xxred") → ""
-        seeColor("blueTimes") → "blue"
+		seeColor("blueTimes") → "blue"
 */
+function seeColor(x) {
+	if (x.substring(0, 3) === "red") {
+		return "red";
+	} else if (x.substring(0, 4) === "blue") {
+		return "blue";
+	} else {
+		return "";
+	}
 
+}
 /*
-5. **oddOnly** Write a function that given an array of integer of any length, filters out 
-    the even number, and returns a new array of just the the odd numbers.
+5. **oddOnly** Write a function that given an array of integer of any length, filters out
+	the even number, and returns a new array of just the the odd numbers.
 
 		oddOnly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) → [1, 3, 5, 7, 9, 11];
 		oddOnly([2, 4, 8, 32, 256]); → []
 */
-
+function oddOnly(x) {
+	let oddArray = [];
+	for (let i = 0; i < x.length; ++i) {
+		if (x[i] % 2 == 1) {
+			oddArray.push(x[i]);
+		}
+	}
+	return oddArray;
+}
 /*
-6. **frontAgain** Given a string, return true if the first 2 chars in the string also appear 
-    at the end of the string, such as with "edited".
+6. **frontAgain** Given a string, return true if the first 2 chars in the string also appear
+	at the end of the string, such as with "edited".
 
 		frontAgain("edited") → true
 		frontAgain("edit") → false
 		frontAgain("ed") → true
 */
+function frontAgain(x) {
+	if (x.substring(0, 2) == x.substring(x.length - 2)) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 /*
-7. **cigarParty** When squirrels get together for a party, they like to have cigars. 
-A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. 
-Unless it is the weekend, in which case there is no upper bound on the number of cigars. 
-Write a squirrel party function that return true if the party with the given values is successful, 
+7. **cigarParty** When squirrels get together for a party, they like to have cigars.
+A squirrel party is successful when the number of cigars is between 40 and 60, inclusive.
+Unless it is the weekend, in which case there is no upper bound on the number of cigars.
+Write a squirrel party function that return true if the party with the given values is successful,
 or false otherwise.
 
 		cigarParty(30, false) → false
 		cigarParty(50, false) → true
 		cigarParty(70, true) → true
 */
-
+function cigarParty(x, y) {
+	if (x >= 40 && y == true) {
+		return true;
+	} else if (x >= 40 && x <= 60) {
+		return true;
+	} else {
+		return false;
+	}
+}
 /*
 8. **fizzBuzz** Because you know you can't live without it, FizzBuzz.
 
@@ -78,6 +129,18 @@ or false otherwise.
 		fizzBuzz(15) → "FizzBuzz"
 		fizzBuzz(8) → 8
 */
+function fizzBuzz(x) {
+	if (x % 3 === 0 && x % 5 === 0) {
+		return "FizzBuzz";
+	} else if (x % 5 === 0) {
+		return "Buzz";
+	} else if (x % 3 === 0) {
+		return "Fizz"
+	} else {
+		return x;
+	}
+}
+
 
 /*
 9. **filterEvens** Write a function that filters an array to only include even numbers.
@@ -88,6 +151,16 @@ or false otherwise.
 	filterEvens([100, 8, 21, 24, 62, 9, 7]) → [100, 8, 24, 62]
 */
 
+function filterEvens(x) {
+	let evenArray = [];
+	for (let i = 0; i < x.length; ++i) {
+		if (x[i] % 2 === 0) {
+			evenArray.push(x[i]);
+		}
+	}
+	return evenArray;
+}
+
 /*
 10. **filterBigNumbers** Write a function that filters numbers greater than or equal to 100.
 
@@ -96,16 +169,38 @@ or false otherwise.
 	filterBigNumbers([]) → []
 */
 
+function filterBigNumbers(x) {
+	let bigNumArray = [];
+	for (let i = 0; i < x.length; ++i) {
+		if (x[i] >= 100) {
+			bigNumArray.push(x[i]);
+		}
+	}
+	return bigNumArray;
+}
+
 /*
-11. **filterMultiplesOfX** Write a function to filter numbers that are a multiple of a 
+11. **filterMultiplesOfX** Write a function to filter numbers that are a multiple of a
 parameter, `x` passed in.
 
 	filterMultiplesOfX([3, 5, 1, 9, 18, 21, 42, 67], 3) → [3, 9, 18, 21, 42]
 	filterMultiplesOfX([3, 5, 10, 20, 18, 21, 42, 67], 5) → [5, 10, 20]
 */
 
+function filterMultiplesOfX(y, x) {
+	let multiplesArray = [];
+	for (let i = 0; i < y.length; ++i) {
+		if ((x % y[i]) == 0) {
+			multiplesArray.push(y[i]);
+		}
+	}
+	return multiplesArray;
+}
+
+
+
 /*
-12. **createObject** Write a function that creates an object with a property called 
+12. **createObject** Write a function that creates an object with a property called
 firstName, lastName, and age. Populate the properties with your values.
 
 	createObject() →
@@ -116,3 +211,11 @@ firstName, lastName, and age. Populate the properties with your values.
 		age
 	}
 */
+
+function createObject(x, y, z) {
+	let me = {
+		firstName: x,
+		lastName: y,
+		age: z
+	};
+}
