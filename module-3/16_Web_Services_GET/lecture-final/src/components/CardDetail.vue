5ef30fc50a7c196a4ae3d8196f8a97293529e912
Dave Hoy <david.hoy@techelevator.com>
2021-12-01 15:31:32 -0500

    M3D18 Lecture final

---
+++ module-3/16_Web_Services_GET/lecture-final/src/components/CardDetail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+import boardService from '@/services/BoardService.js';
+
+export default {
+  name: "card-detail",
+  data() {
+    return {
+      title: '',
+      description: ''
+    }
+  },
+  created() {
+    let boardID = this.$route.params.boardID;
+    let cardID = this.$route.params.cardID;
+    boardService.getCard(boardID, cardID).then(response =>
+    {
+      this.title = response.title;
+      this.description = response.description;
+    });
+  }
+};
+</script>
